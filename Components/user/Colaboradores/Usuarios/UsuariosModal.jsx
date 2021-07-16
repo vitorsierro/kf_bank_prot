4ceadfec4f19cca53ff3e01bb6697a29e22ec3c1
Vitor Sierro <vitorsierro_@hotmail.com>
2021-07-16 12:34:51 -0300

    'repareErroLinkNext'

--- Components/user/Colaboradores/Usuarios/UsuariosModal.jsx
+++ Components/user/Colaboradores/Usuarios/UsuariosModal.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Link from 'next/Link';
+import Link from 'next/link';
 //CSS
 import { Row, Col, Input, Form, Select, Radio, Checkbox } from "antd";
 import styled from '../../../../styles/Transferencia.module.css';
